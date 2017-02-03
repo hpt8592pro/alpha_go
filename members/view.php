a35d45e79d71b4a1d6adc205cb461a12b005aef3
Hardik Trivedi <hardik@webmavens.in>
2017-02-04 01:15:13 +0530

    test

--- members/view.php
+++ members/view.php
@@ -15,9 +15,23 @@
 		<div class="form-group">
 			<label for="inputlg">GO Link</label>
 			<input class="form-control input-lg selected" id="inputlg" name="go_link" type="text" value="http://go.alphanuke.com/to/<?=$id?>" selected>
+			<div id="selectable" onclick="selectText('selectable')">http://example.com/page.htm</div>
 		</div>
 	</div><!--container -->
 	<br><br><br><br><br><br>
+	<script type="text/javascript">
+        function selectText(containerid) {
+            if (document.selection) {
+                var range = document.body.createTextRange();
+                range.moveToElementText(document.getElementById(containerid));
+                range.select();
+            } else if (window.getSelection) {
+                var range = document.createRange();
+                range.selectNode(document.getElementById(containerid));
+                window.getSelection().addRange(range);
+            }
+        }
+	</script>
 <?php
 	require '../parts/footer.php';
 ?>
