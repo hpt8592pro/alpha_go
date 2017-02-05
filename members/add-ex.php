256876a02d8ac61474d36a95ee8153db5d90fb6a
Hardik Trivedi <hardik@webmavens.in>
2017-02-05 18:39:50 +0530

    Fix

--- members/add-ex.php
+++ members/add-ex.php
@@ -12,18 +12,10 @@
 		$title = $_POST['go_title'];
 		$pass = $_POST['go_pass'];
 		$user = $_SESSION['user'];
-		echo "<pre>";
-		print_r($_SESSION);
-		echo "</pre>";
-		echo "<pre>";
-		var_dump($user);
-		echo "</pre>";
-		echo "<pre>";
-		var_dump($_SESSION);
-		echo "</pre>";die;
+		$user_id = $user['user'];
 		$time = date("Y-m-d h:i:s");
 		$hash = date("ymdhis");
-		$sql = "INSERT INTO `links` (`hash`, `title`, `link`, `user`, `password`, `time`) VALUES ('".$hash."', '".$title."', '".$link."', '".$user."', '".$pass."', '".$time."')";
+		$sql = "INSERT INTO `links` (`hash`, `title`, `link`, `user`, `password`, `time`) VALUES ('".$hash."', '".$title."', '".$link."', '".$user_id."', '".$pass."', '".$time."')";
 		$exec = mysqli_query($con, $sql);
 		header("Location: view.php?from=ex&agent=$hash");
 	}
